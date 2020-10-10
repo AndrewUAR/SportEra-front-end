79bc47d3e478b38d1d948431bac5d12cbec0ae37
Andrew <andrewriznykua93@gmail.com>
2020-10-09 23:29:18 -0500

    rootReducer refactored

--- src/app/reducers/rootReducer.ts
+++ src/app/reducers/rootReducer.ts
@@ -1,24 +1,37 @@
-import { CombinedState, combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist';
+import { AnyAction, CombinedState, combineReducers, Reducer } from 'redux';
+import { PersistConfig, persistReducer, PersistState } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { AuthActionTypes, LOGOUT } from '../constants/auth';
+import { AuthActionTypes, LOGOUT } from '../constants/authTypes';
+import { ErrorActionTypes } from '../constants/errorTypes';
+import { IAuthState } from '../states/authState';
+import { IErrorState } from '../states/errorState';
 import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 
-const persistConfig = {
+export type RootState = {
+    auth: IAuthState;
+    error: IErrorState;
+    _persist: PersistState;
+};
+
+export type RootAction = AuthActionTypes | ErrorActionTypes;
+
+export type RootReducer = Reducer<RootState, RootAction>;
+
+const persistConfig: PersistConfig<RootReducer> = {
     key: 'root',
     storage,
 };
 
-export const appReducers = combineReducers({
+export const appReducers = combineReducers<RootReducer>({
     auth: authReducer,
+    error: errorReducer,
 });
 
-export type RootState = ReturnType<typeof appReducers>;
-
-const rootReducer = (state: CombinedState<RootState> | undefined, action: AuthActionTypes) => {
+const rootReducer = (state: RootReducer | undefined, action: RootAction) => {
     if (action.type === LOGOUT) {
-        state = undefined;
         localStorage.clear();
+        return appReducers(undefined, action);
     }
     return appReducers(state, action);
 };
