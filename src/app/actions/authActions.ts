b34197461e73777933817cd931164eb3a8aad491
Andrew <andrewriznykua93@gmail.com>
2020-10-09 23:30:45 -0500

    authReducer error dispatch added

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -3,29 +3,29 @@ import { ThunkAction } from 'redux-thunk';
 import {
     AUTHENTICATE_USER,
     IAuthenticateUserAction,
-    IRegisterUserAction,
-    REGISTER_USER,
     UserLoginState,
     UserRegisterState,
-} from '../constants/auth';
-import { signInUser, signUpUser } from '../services/auth.service';
+} from '../constants/authTypes';
+import { forgotPassword, signInUser, signUpUser } from '../services/auth.service';
 import { RootState } from '../reducers/rootReducer';
+import { SET_ERROR } from '../constants/errorTypes';
 
 export const registerUser: ActionCreator<ThunkAction<
     Promise<void>,
     RootState,
     UserRegisterState,
-    IRegisterUserAction
+    IAuthenticateUserAction
 >> = (userData: UserRegisterState) => async (dispatch: Dispatch) => {
     try {
         await signUpUser(userData);
-        const message = 'Your account was successfully created. Verification email was sent to your email address.';
-        dispatch({
-            type: REGISTER_USER,
-            payload: message,
-        });
     } catch (error) {
-        console.log(error);
+        dispatch({
+            type: SET_ERROR,
+            payload: {
+                error: error.response.data.message,
+                validationErrors: error.response.data.validationErrors,
+            },
+        });
     }
 };
 
@@ -42,6 +42,31 @@ export const authenticateUser: ActionCreator<ThunkAction<
             payload: user.data,
         });
     } catch (error) {
-        console.log(error);
+        dispatch({
+            type: SET_ERROR,
+            payload: {
+                error: error.response.data.message,
+                validationErrors: error.response.data.validationErrors,
+            },
+        });
+    }
+};
+
+export const forgotUserPassword: ActionCreator<ThunkAction<
+    Promise<void>,
+    RootState,
+    { userEmail: string },
+    IAuthenticateUserAction
+>> = (userEmail: string) => async (dispatch: Dispatch) => {
+    try {
+        await forgotPassword(userEmail);
+    } catch (error) {
+        dispatch({
+            type: SET_ERROR,
+            payload: {
+                error: error.response.data.message,
+                validationErrors: error.response.data.validationErrors,
+            },
+        });
     }
 };
