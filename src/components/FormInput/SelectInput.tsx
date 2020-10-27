ee6669d585c1c07e9a35f4caafb8d382b236e4ba
Andrew <andrewriznykua93@gmail.com>
2020-10-26 22:58:31 -0500

    select input refactored

--- src/components/FormInput/SelectInput.tsx
+++ src/components/FormInput/SelectInput.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select, { ActionMeta, GroupedOptionsType, OptionsType, OptionTypeBase, ValueType } from 'react-select';
 import PropTypes, { InferProps } from 'prop-types';
 import classNames from 'classnames';
 import { Form } from 'react-bootstrap';
@@ -7,13 +8,16 @@ import styles from '../../assets/jss/components/formInputStyles.jss';
 
 const useStyles = createUseStyles(styles);
 
-type SelectInputProps = {
-    controlId: string;
+interface SelectInputProps<OptionType extends OptionTypeBase = { label: string; value: string }> {
     label: string;
-    value: string;
+    options: OptionsType<OptionType>;
+    isLoading: boolean;
+    isDisabled: boolean;
+    isMulti: boolean;
     name: string;
+    onChange: (value: ValueType<OptionType>, actionMeta: ActionMeta<OptionType>) => void;
     color?: 'blue' | 'dark';
-};
+}
 
 const defaultProps = {
     color: 'blue' as 'blue' | 'dark',
@@ -22,7 +26,7 @@ const defaultProps = {
 type InferredProps = InferProps<typeof SelectInput.propTypes>;
 
 const SelectInput: React.FC<SelectInputProps> = (props: InferredProps & SelectInputProps) => {
-    const { controlId, name, value, label, color = 'blue' } = props;
+    const { name, options, label, isMulti, color = 'blue', isLoading, isDisabled, onChange } = props;
     const classes = useStyles();
 
     const inputClasses = classNames({
@@ -33,12 +37,15 @@ const SelectInput: React.FC<SelectInputProps> = (props: InferredProps & SelectIn
     return (
         <Form.Group className={classes.formGroup}>
             <Form.Label className={classes.label}>{label}</Form.Label>
-            <Form.Control as="select" className={inputClasses} id={controlId} custom>
-                <option value="0">Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </Form.Control>
+            <Select
+                defaultValue={options[0]}
+                isMulti={isMulti}
+                isLoading={isLoading}
+                isDisabled={isDisabled}
+                name={name}
+                options={options}
+                onChange={onChange}
+            />
         </Form.Group>
     );
 };
@@ -46,9 +53,7 @@ const SelectInput: React.FC<SelectInputProps> = (props: InferredProps & SelectIn
 SelectInput.defaultProps = defaultProps;
 
 SelectInput.propTypes = {
-    controlId: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     color: PropTypes.oneOf(['blue', 'dark']),
 };
